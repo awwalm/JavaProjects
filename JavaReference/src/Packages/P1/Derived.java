394198e00add087adcaba3e04a4148b630f6b887
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-06-07 23:52:11 +0400

    Added Packages

---
+++ JavaReference/src/Packages/P1/Derived.java
@@ -0,0 +1,14 @@
+package Packages.P1;
+
+public class Derived extends Protection {
+    Derived() {
+        System.out.println("derived constructor");
+        System.out.println("n = " + n);
+
+        // class only
+        // System.out.println("n = " + n);
+
+        System.out.println("n_pro = " + n_pro);
+        System.out.println("n_pub = " + n_pub);
+    }
+}
